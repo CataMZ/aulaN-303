f0bd080417c2bccff7bef6962a920851a206acf9
Catarina <catarina.manzieri@gmail.com>
2019-02-21 21:41:34 -0300

    Aprender sobre orientacao de objetos no Python

---
+++ aulaDiaTres.py
@@ -0,0 +1,46 @@
+# class Bolo:
+#     tamanho = 'grande'
+
+    # def __init__(instancia, sabor, cobertura, tamanho):
+    #     instancia.sabor = sabor
+    #     instancia.cobertura = cobertura
+    #     instancia.tamanho = tamanho
+
+    # def __init__(instancia, **kwargs):
+    #     sabor = kwargs.get('sabor')
+    #     tamanho = kwargs.get('tamanho')
+
+    #     if sabor is not None:
+    #         instancia.sabor = sabor
+    #     if tamanho is not None:
+    #         instancia.tamanho = tamanho
+
+#     def __init__(self, sabor, cobertura):
+#         self.sabor = sabor
+#         self.cobertura = cobertura
+
+#     def assar(self):
+#         print('O bolo está assando!')
+
+#     def comer(self):
+#         pass
+
+#     def esta_estragado(self):
+#         pass
+
+
+# bolo1 = Bolo('chocolate','chocolate')
+# bolo2 = Bolo('chocolate', 'leite condensado')
+
+# bolo1.assar()
+
+# print(bolo1.sabor)
+# print(bolo1.tamanho)
+
+# print(bolo2.sabor)
+# print(bolo2.tamanho)
+
+# if bolo1.sabor == bolo2.sabor:
+#     print('Voce tem dois bolos com sabores iguais!')
+# else:
+#     print('Os bolos tem sabores diferentes')
